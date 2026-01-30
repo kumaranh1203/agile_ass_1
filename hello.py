687e7b9a3f8ae58f0f07fa390325c7e0cec6eca6
kumaranh1203 <kumaran.h2023@vitstudent.ac.in>
2026-01-30 18:14:54 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+name = "Kumaran"
+reg_no = "23MIS0318"
+
+print("Student Information")
+print("---------------------")
+print("Name      :", name)
+print("Register No:", reg_no)
+
+print("\nWelcome to Python programming!")
+print("This file was created as a basic demo.")
